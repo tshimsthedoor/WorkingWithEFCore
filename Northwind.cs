37a2c52c571c6992cccf2e1c6e82d475113fe54b
Tresor Tshimanga <tshims79@gmail.com>
2022-03-15 17:36:18 +0200

    I create the model to map top the Northwind database

--- Northwind.cs
+++ Northwind.cs
@@ -1,10 +1,30 @@
 using System;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace Packt.Shared
 {
-    public class Northwind
+
+    // this manage the connection to the database
+    public class Northwind :DbContext
     {
-        
+        // thsese properties map to the table in the database
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // example of using Fluent API instead of attributes
+            // to limit the length of a category name to 15
+            modelBuilder.Entity<Category>()
+                    .Property(category => category.CategoryName)
+                    .IsRequired() //Not null
+                    .HasMaxLength(15);
+        }
     }
 }

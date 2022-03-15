2bc922353db63841ac35760739ca02465ead7fcb
Tresor Tshimanga <tshims79@gmail.com>
2022-03-15 17:48:42 +0200

    Now we can run the project and return the categories and list of product

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
-namespace WorkingWithEFCore
+namespace Packt.Shared
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           QueryingCategories();
+        }
+
+        static void QueryingCategories()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("Categories and How many products they have:");
+                // a query to get all categories and their related products
+                IQueryable<Category> cats = db.Categories.Include(c => c.Products);
+
+                foreach(Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+                }
+
+            }
         }
     }
 }

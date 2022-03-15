e25439918e2d67039e18cdfd08ade5755c5de9a0
Tresor Tshimanga <tshims79@gmail.com>
2022-03-15 16:49:02 +0200

    Add property and contructor
    - Added property to map to the category database and I also create the constructor

--- Category.cs
+++ Category.cs
@@ -1,10 +1,24 @@
 using System;
-
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Packt.Shared
 {
     public class Category
     {
         // these properties map to colums in the database
-    }
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+        
+        // defines a navigation property for related rows
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Category(){
+            // to enable developpers to add products to a category we must
+            // initialize the navigation property to an empty list
+            this.Products = new List<Product>();
+        }
 }
